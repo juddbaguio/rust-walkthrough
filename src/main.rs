fc060517b5cc695642cac692bca3de4879bc9a8d
Judd Misael R. Baguio <juddmisaelbaguio@gmail.com>
2022-08-01 22:13:04 +0800

    changes

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-fn double_the_num(mut x: i32) -> String {
+fn double_the_num(format_str: &str, mut x: i32) -> String {
     x = x * 2;
 
-    let res_string = "the doubled num is ".to_string() + &x.to_string();
+    let res_string = format_str.to_string() + &x.to_string();
     return res_string;
 }
 
 fn main() {
-    println!("{}", double_the_num(2));
+    println!("{}", double_the_num("the num doubled is ", 2));
 }
