dd8ec85ddc1296c6e98e3978bcb763041192c289
Judd Misael R. Baguio <juddmisaelbaguio@gmail.com>
2022-08-01 22:09:34 +0800

    with sample functions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
+fn double_the_num(mut x: i32) -> String {
+    x = x * 2;
+
+    let res_string = "the doubled num is ".to_string() + &x.to_string();
+    return res_string;
+}
 
 fn main() {
-
+    println!("{}", double_the_num(2));
 }
